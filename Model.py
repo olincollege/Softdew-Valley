64345c95ebdea6e02064376728865a7063e11dc3
amgeorge22 <ageorge@olin.edu>
2023-04-28 14:15:44 -0400

    Move information from main to a model class

---
+++ Model.py
@@ -0,0 +1,61 @@
+import music
+from FarmerClass import Farmer
+from ViewClass import View
+from GroundClass import Ground
+from GameState import GameState
+from EquipmentClass import (
+    WateringCan,
+    Hoe,
+    Seed,
+    ParsnipSeeds,
+    CauliflowerSeeds,
+    Crop,
+    Parsnip_Crop,
+    Cauliflower_Crop,
+)
+from Inventory_Class import Inventory
+from plants import Plants
+
+
+class Model:
+    def __init__(self):
+        self.farmer = Farmer  # should probably make this an instance?
+        self.ground = Ground()
+        self.gamestate = GameState(self.farmer, self.ground)
+        self.watering_can = WateringCan(self.gamestate)
+        self.hoe = Hoe(self.gamestate)
+        self.parsnipseeds = ParsnipSeeds(self.gamestate)
+        self.cauliflowerseeds = CauliflowerSeeds(self.gamestate)
+        self.inventory = Inventory(
+            self.watering_can,
+            self.hoe,
+            self.parsnipseeds,
+            self.cauliflowerseeds,
+        )
+
+    def perform_action(self):
+        equipped_item = self.inventory.get_equipped_item()
+        if equipped_item is not None:
+            # the action function is different for each item
+            equipped_item.action()
+
+    def update_equipped_inventory(self, mouse_pos):
+        if (
+            mouse_pos[0] > View.INVENTORY_START_WIDTH
+            and mouse_pos[0] < View.INVENTORY_START_WIDTH + 7 * 50
+        ):
+            if (
+                mouse_pos[1] > View.INVENTORY_START_HEIGHT
+                and mouse_pos[1]
+                < View.INVENTORY_START_HEIGHT + View.GROUND_SIZE
+            ):
+                self.inventory.control_inventory(mouse_pos)
+
+    def day_passes(self):
+        rows = self.ground.num_rows
+        cols = self.ground.num_cols
+        for j in range(cols):
+            for i in range(rows):
+                if isinstance(self.ground.land[i][j], Plants):
+                    self.ground.land[i][j].grow()
+        self.ground.unwater_squares()
--- main.py
+++ main.py
@@ -1,37 +1,18 @@
 import pygame
 
 import music
-from FarmerClass import Farmer
+
 from ViewClass import View
-from GroundClass import Ground
-from GameState import GameState
-from EquipmentClass import (
-    WateringCan,
-    Hoe,
-    Seed,
-    ParsnipSeeds,
-    CauliflowerSeeds,
-    Crop,
-    Parsnip_Crop,
-    Cauliflower_Crop,
-)
-from Inventory_Class import Inventory
 from plants import Plants
+from Model import Model
 
 FPS = 60
 
 
 def main():
     pygame.init()
-    farmer = Farmer  # should probably make this an instance?
-    ground = Ground()
-    gamestate = GameState(farmer, ground)
-    watering_can = WateringCan(gamestate)
-    hoe = Hoe(gamestate)
-    parsnipseeds = ParsnipSeeds(gamestate)
-    cauliflowerseeds = CauliflowerSeeds(gamestate)
-    inventory = Inventory(watering_can, hoe, parsnipseeds, cauliflowerseeds)
-    display_farmer = View(farmer, ground, gamestate, inventory)
+    model = Model()
+    display = View(model.farmer, model.ground, model.gamestate, model.inventory)
     clock = pygame.time.Clock()
     game_running = True
     music.play_music()
@@ -45,43 +26,23 @@ def main():
                 music.play_music()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    equipped_item = inventory.get_equipped_item()
-                    if equipped_item is not None:
-                        # the action function is different for each item
-                        equipped_item.action()
+                    model.perform_action()
                 for i in range(1, 9):
                     if event.key == getattr(pygame, f"K_{i}"):
-                        inventory.control_inventory(num=i - 1)
+                        model.inventory.control_inventory(num=i - 1)
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_h:
-                    gamestate.harvest_crop(inventory)
+                    model.gamestate.harvest_crop(model.inventory)
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = pygame.mouse.get_pos()
-                if (
-                    mouse_pos[0] > View.INVENTORY_START_WIDTH
-                    and mouse_pos[0] < View.INVENTORY_START_WIDTH + 7 * 50
-                ):
-                    if (
-                        mouse_pos[1] > View.INVENTORY_START_HEIGHT
-                        and mouse_pos[1]
-                        < View.INVENTORY_START_HEIGHT + View.GROUND_SIZE
-                    ):
-                        inventory.control_inventory(mouse_pos)
-
+                model.update_equipped_inventory(mouse_pos)
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_p:
-                    # print("you hit p")
-                    rows = ground.num_rows
-                    cols = ground.num_cols
-                    for j in range(cols):
-                        for i in range(rows):
-                            if isinstance(ground.land[i][j], Plants):
-                                ground.land[i][j].grow()
-                    ground.unwater_squares()
+                    model.day_passes()
 
         keys_pressed = pygame.key.get_pressed()
-        farmer.move(farmer, keys_pressed)
-        display_farmer.draw_window()
+        model.farmer.move(model.farmer, keys_pressed)
+        display.draw_window()
     pygame.quit()
 
 
