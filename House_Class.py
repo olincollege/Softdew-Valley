687e42aadc051533f6a9e2d8392b982c48469ef4
Meagan Lipsman <meaganlipsman@gmail.com>
2023-04-30 20:25:29 -0400

    Added to House_Class to make make an event

--- House_Class.py
+++ House_Class.py
@@ -1,7 +1,10 @@
 """
 House Class
 """
-import Day_Class
+import pygame
+from ViewClass import View
+from FarmerClass import Farmer
+
 
 class House:
     """
@@ -13,17 +16,29 @@ class House:
 
     """
 
+    ENTER_HOUSE = pygame.USEREVENT + 2
+    WIDTH, HEIGHT = View.WIDTH, View.HEIGHT
+    house_start_square_x = WIDTH - 250
+    house_start_square_y = 200
+    house_rect = pygame.Rect(
+        house_start_square_x, house_start_square_y, 250, 200
+    )
+
     def __init__(self):
         """
         Creates house attribute which contains house squares.
         """
 
-        self._house = ["H","H","H"] #don't think this is right 
+        self._house = ["H", "H", "H"]  # don't think this is right
 
     def enter_house(self):
         """
         When the player steps on one of the house squares they "enter" the
         house and that triggers the player to sleep and start a new day.
         """
-        if #current_square = self._house  ??not sure 
-            self._day_number = self._day_number + 1
+        print("calling")
+        collide = pygame.Rect.colliderect(self.house_rect, Farmer.farmer_rect)
+        if collide:
+            pygame.event.post(pygame.event.Event(self.ENTER_HOUSE))
+            print("They've collided")
+            # self._day_number = self._day_number + 1
